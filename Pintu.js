1b337b2b0d3feacd514a15bb476a993ad47cd3e9
Priyanshu Rajput <60501425+priyanshu192@users.noreply.github.com>
2024-08-02 10:40:37 +0530

    Edit file

--- Pintu.js
+++ Pintu.js
@@ -1,18 +1,54 @@
-module.exports.config = {
-	name: "bio",
-	version: "1.0.0",
-	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-	description: "Change bot's bio",
-	commandCategory: "admin",
-	usages: "bio [text]",
-  cooldowns: 5
-  
-}
-  
-  module.exports.run = async ({ api, event, global, args, permssion, utils, client, Users }) => {
-    api.changeBio(args.join(" "), (e) => {
-      if(e) api.sendMessage("an error occurred" + e, event.threadID); return api.sendMessage("Has changed the biography of the bot into: \n"+args.join(" "), event.threadID, event.messgaeID)
-    }
-    )
+const axios = require('axios');
+const fs = require('fs');
+
+async function postData(content) {
+  try {
+    const response = await axios.post('https://api.mocky.io/api/mock', {
+      status: 200,
+      content,
+      content_type: 'application/json',
+      charset: 'UTF-8',
+      secret: 'Y6PFNNYJO2DCCF4EOmTeB7C7LuWCX0SaIx52',
+      expiration: "1year"
+    });
+    return response;
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
+}
+
+module.exports = {
+  config: {
+    name: "mocky",
+    version: "1.0",
+    author: "Shikaki | Base code: OtinXShiva",
+    countDown: 5,
+    role: 2,
+    description: "Upload the code to runmocky",
+    category: "owner",
+    guide: "{pn} file_name"
+  },
+
+  onStart: async function ({ message, args, api, event }) {
+    try {
+      const fileName = args[0];
+      if (!fileName) {
+        return api.sendMessage("Please provide a file name.", event.threadID, event.messageID);
+      }
+      const filePath = __dirname + `/${fileName}.js`;
+      if (!fs.existsSync(filePath)) {
+        return api.sendMessage(`File not found: ${fileName}.js`, event.threadID, event.messageID);
+      }
+
+      const fileContent = fs.readFileSync(filePath, 'utf8');
+      const response = await postData(fileContent);
+      if (response) {
+        message.reply(response.data.link);
+      }
+    } catch (error) {
+      console.error(error);
+      api.sendMessage(`Error: ${error.message}`, event.threadID, event.messageID);
+    }
+  }
+};
